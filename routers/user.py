e85eae06a6abfa735c213a1103e2bd27f58c17df
Niger Corella <ncorella@hotmail.com>
2022-09-14 01:13:12 +0200

    Cnx a base de Datos

--- routers/user.py
+++ routers/user.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter
+from config.db import cnx
+from models.Modeluser import users
+from schemas.users import User
 
 user = APIRouter()
 
 @user.get("/")
 def home():
-    return "Pagina Principal"
+    return cnx.execute(users.select()).fetchall()
 
-@user.get("/users/id")
-def Details():
-    return "Pagina Principal"
+@user.post("/users/")
+def createUser(user:User):
+    return "El usuario  se creo correctamente"
 
 @user.get("/users/id")
 def Update():
