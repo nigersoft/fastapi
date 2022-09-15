3611b93b8a1b66e02e0b02784c392ebaa8074b39
Niger Corella <ncorella@hotmail.com>
2022-09-16 01:08:03 +0200

    Proyecto terminado solo el crud

--- models/Modeluser.py
+++ models/Modeluser.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql.sqltypes import Integer, String
 
 from config.db import meta, engine
 
-users = Table('users',meta,
+ModelUsers = Table('users',meta,
 Column('id',Integer(),primary_key=True),
 Column('name', String(255)),
 Column('email',String(255)),
--- routers/user.py
+++ routers/user.py
@@ -1,18 +1,42 @@
+from winreg import REG_RESOURCE_REQUIREMENTS_LIST
+from xmlrpc.server import resolve_dotted_attribute
 from fastapi import APIRouter
 from config.db import cnx
-from models.Modeluser import users
+from models.Modeluser import ModelUsers
 from schemas.users import User
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+f = Fernet(key)
+
 
 user = APIRouter()
 
 @user.get("/")
 def home():
-    return cnx.execute(users.select()).fetchall()
+    return cnx.execute(ModelUsers.select()).fetchall()
 
 @user.post("/users/")
-def createUser(user:User):
-    return "El usuario  se creo correctamente"
+def createUser(puser:User):
+    newUser = {"name":puser.name,
+    "email":puser.email,
+    "password":f.encrypt(puser.password.encode("utf-8"))}
+    result = cnx.execute(ModelUsers.insert().values(newUser))
+    return cnx.execute(ModelUsers.select().where(ModelUsers.c.id == result.lastrowid)).first()
 
-@user.get("/users/id")
-def Update():
-    return "Pagina Principal"    
+@user.get("/users/{id}")
+def UserDetails(id:str):
+    return cnx.execute(ModelUsers.select().where(ModelUsers.c.id == id)).first()
+
+@user.delete("/users/{id}")
+def Delete(id:int):
+    result = cnx.execute(ModelUsers.delete().where(ModelUsers.c.id == id))
+    return "Usuario Eliminado"    
+
+@user.put("/users/{id}")
+def Update(id:int,puser:User):
+    newUser = {"name":puser.name,
+    "email":puser.email,
+    "password":f.encrypt(puser.password.encode("utf-8"))}
+    result = cnx.execute(ModelUsers.update().values(newUser).where(ModelUsers.c.id == id))    
+    return cnx.execute(ModelUsers.select().where(ModelUsers.c.id == id)).first()
